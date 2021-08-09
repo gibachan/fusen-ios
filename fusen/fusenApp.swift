a23cb34cd167b9adbad3d864371752cc922c51a6
gibachan <childhoodend.tk@gmail.com>
2021-08-10 08:47:03 +0900

    Init project

---
+++ fusen/fusenApp.swift
@@ -0,0 +1,17 @@
+//
+//  fusenApp.swift
+//  fusen
+//
+//  Created by Tatsuyuki Kobayashi on 2021/08/10.
+//
+
+import SwiftUI
+
+@main
+struct fusenApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

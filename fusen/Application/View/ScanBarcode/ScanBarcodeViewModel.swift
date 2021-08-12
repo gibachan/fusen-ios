0c87a9dc68bf3d7ea84747d4b1d588099fd502a1
gibachan <childhoodend.tk@gmail.com>
2021-08-12 20:59:17 +0900

    Ignore redundant scanning

--- fusen/Application/View/ScanBarcode/ScanBarcodeViewModel.swift
+++ fusen/Application/View/ScanBarcode/ScanBarcodeViewModel.swift
@@ -10,6 +10,8 @@ import Foundation
 
 final class ScanBarcodeViewModel: ObservableObject {
     private let scanInterval: Double = 1.0
+    private var isScanning = false
+    private var scannedCode = Set<String>()
     private var lastScannedTime = Date(timeIntervalSince1970: 0)
     private let publicationRepository: PublicationRepository
 
@@ -31,19 +33,22 @@ final class ScanBarcodeViewModel: ObservableObject {
     }
 
     func onBarcodeScanned(code: String) async {
-        guard let isbn = ISBN.from(code: code) else {
-            return
-        }
+        guard !isScanning else { return }
+        guard !scannedCode.contains(code) else { print("#passed"); return }
+        guard let isbn = ISBN.from(code: code) else { return }
         
         let now = Date()
         if now.timeIntervalSince(lastScannedTime) >= scanInterval {
+            isScanning = true
             lastScannedTime = now
+            scannedCode.insert(code)
             do {
                 let publication = try await publicationRepository.findBy(isbn: isbn)
                 print("Publication found: \(publication)")
             } catch {
                 print(error.localizedDescription)
             }
+            isScanning = false
         }
     }
     

021b3858c2ab4aa2f9e7dc77660eceb94e487b44
gibachan <childhoodend.tk@gmail.com>
2021-08-19 12:17:30 +0900

    Fix file position

--- fusen/Application/Utility/LoadingHUD.swift
+++ fusen/Application/Utility/LoadingHUD.swift
@@ -6,3 +6,14 @@
 //
 
 import Foundation
+import SVProgressHUD
+
+final class LoadingHUD {
+    static func show() {
+        SVProgressHUD.show()
+    }
+    
+    static func dismiss() {
+        SVProgressHUD.dismiss()
+    }
+}

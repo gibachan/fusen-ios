c017f66362d49a20ef14eeb6e862c4cc43430f50
gibachan <childhoodend.tk@gmail.com>
2021-09-05 15:04:34 +0900

    Refactor HomeTab

--- fusen/Application/View/HomeTab/HomeTabViewModel.swift
+++ fusen/Application/View/HomeTab/HomeTabViewModel.swift
@@ -12,24 +12,18 @@ final class HomeTabViewModel: ObservableObject {
     private let latestBooksCount = 4
     private let latestMemosCount = 4
     
-    private let accountService: AccountServiceProtocol
-    private let userRepository: UserRepository
-    private let bookRepository: BookRepository
-    private let memoRepository: MemoRepository
+    private let getReadingBookUseCase: GetReadingBookUseCase
+    private let getLatestDataUseCase: GetLatestDataUseCase
     
     @Published var state: State = .initial
     @Published var readingBook: Book?
     
     init(
-        accountService: AccountServiceProtocol = AccountService.shared,
-        userRepository: UserRepository = UserRepositoryImpl(),
-        bookRepository: BookRepository = BookRepositoryImpl(),
-        memoRpository: MemoRepository = MemoRepositoryImpl()
+        getReadingBookUseCase: GetReadingBookUseCase = GetReadingBookUseCaseImpl(),
+        getLatestDataUseCase: GetLatestDataUseCase = GetLatestDataUseCaseImpl()
     ) {
-        self.accountService = accountService
-        self.userRepository = userRepository
-        self.bookRepository = bookRepository
-        self.memoRepository = memoRpository
+        self.getReadingBookUseCase = getReadingBookUseCase
+        self.getLatestDataUseCase = getLatestDataUseCase
     }
     
     func onAppear() async {
@@ -41,31 +35,17 @@ final class HomeTabViewModel: ObservableObject {
     }
     
     private func loadAll() async {
-        guard let user = accountService.currentUser else { return }
         guard !state.isInProgress else { return }
         
         state = .loading
         do {
-            let userInfo = try await userRepository.getInfo(for: user)
+            self.readingBook = try? await getReadingBookUseCase.invoke()
             
-            async let books = bookRepository.getLatestBooks(count: latestBooksCount, for: user)
-            async let memos = memoRepository.getLatestMemos(count: latestMemosCount, for: user)
-            let result = try await (books: books, memos: memos)
-            if let readingBookId = userInfo.readingBookId {
-                do {
-                    readingBook = try await bookRepository.getBook(by: readingBookId, for: user)
-                } catch {
-                    // Reaches when the readingBook has been already deleted which is unexpected situation.
-                    log.e(error.localizedDescription)
-                    readingBook = nil
-                }
-            } else {
-                readingBook = nil
-            }
-            if result.books.isEmpty && result.memos.isEmpty {
+            let latestData = try await getLatestDataUseCase.invoke(booksCount: latestBooksCount, memosCount: latestMemosCount)
+            if latestData.books.isEmpty && latestData.memos.isEmpty {
                 state = .empty
             } else {
-                state = .loaded(latestBooks: result.books, latestMemos: result.memos)
+                state = .loaded(latestBooks: latestData.books, latestMemos: latestData.memos)
             }
         } catch {
             log.e(error.localizedDescription)

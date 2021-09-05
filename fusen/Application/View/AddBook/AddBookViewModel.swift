4ddb0f1a0b868e318b10802d4eca5ed6847dbc9c
gibachan <childhoodend.tk@gmail.com>
2021-09-05 19:34:19 +0900

    Refactor AddBook

--- fusen/Application/View/AddBook/AddBookViewModel.swift
+++ fusen/Application/View/AddBook/AddBookViewModel.swift
@@ -9,40 +9,30 @@ import Foundation
 
 @MainActor
 final class AddBookViewModel: ObservableObject {
-    private let accountService: AccountServiceProtocol
     private let analyticsService: AnalyticsServiceProtocol
-    private let bookRepository: BookRepository
+    private let addBookByManualUseCase: AddBookByManualUseCase
 
     @Published var isSaveEnabled = false
     @Published var state: State = .initial
 
     init(
-        accountService: AccountServiceProtocol = AccountService.shared,
         analyticsService: AnalyticsServiceProtocol = AnalyticsService.shared,
-        bookRepository: BookRepository = BookRepositoryImpl()
+        addBookByManualUseCase: AddBookByManualUseCase = AddBookByManualUseCaseImpl()
     ) {
-        self.accountService = accountService
         self.analyticsService = analyticsService
-        self.bookRepository = bookRepository
+        self.addBookByManualUseCase = addBookByManualUseCase
     }
     
     func onTextChange(title: String, author: String) {
-        isSaveEnabled = !title.isEmpty
+        isSaveEnabled = title.isNotEmpty
     }
     
     func onSave(title: String, author: String, thumbnailImage: ImageData?, collection: Collection?) async {
-        guard let user = accountService.currentUser else { return }
         guard !state.isInProgress else { return }
-        
-        let publication = Publication(
-            title: title,
-            author: author,
-            thumbnailURL: nil
-        )
 
         state = .loading
         do {
-            let id = try await bookRepository.addBook(of: publication, in: collection, image: thumbnailImage, for: user)
+            let id = try await addBookByManualUseCase.invoke(title: title, author: author, thumbnailImage: thumbnailImage, collection: collection)
             log.d("Book is added for id: \(id.value)")
             state = .succeeded
             // 強制的に更新 -> Viewの再構築が発生するため注意

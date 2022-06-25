5b682106d6ec44dc6268ad6303a6f1f2faed3d32
gibachan <childhoodend.tk@gmail.com>
2022-06-25 21:29:29 +0900

    Fix to build widget

--- fusenWidget/FusenWidget.swift
+++ fusenWidget/FusenWidget.swift
@@ -1,25 +1,29 @@
 //
-//  fusenWidget.swift
+//  FusenWidget.swift
 //  fusenWidget
 //
 //  Created by Tatsuyuki Kobayashi on 2022/06/25.
 //
 
-import WidgetKit
-import SwiftUI
 import Intents
+import SwiftUI
+import WidgetKit
 
 struct Provider: IntentTimelineProvider {
     func placeholder(in context: Context) -> SimpleEntry {
         SimpleEntry(date: Date(), configuration: ConfigurationIntent())
     }
 
-    func getSnapshot(for configuration: ConfigurationIntent, in context: Context, completion: @escaping (SimpleEntry) -> ()) {
+    func getSnapshot(for configuration: ConfigurationIntent,
+                     in context: Context,
+                     completion: @escaping (SimpleEntry) -> Void) {
         let entry = SimpleEntry(date: Date(), configuration: configuration)
         completion(entry)
     }
 
-    func getTimeline(for configuration: ConfigurationIntent, in context: Context, completion: @escaping (Timeline<Entry>) -> ()) {
+    func getTimeline(for configuration: ConfigurationIntent,
+                     in context: Context,
+                     completion: @escaping (Timeline<Entry>) -> Void) {
         var entries: [SimpleEntry] = []
 
         // Generate a timeline consisting of five entries an hour apart, starting from the current date.
@@ -40,7 +44,7 @@ struct SimpleEntry: TimelineEntry {
     let configuration: ConfigurationIntent
 }
 
-struct fusenWidgetEntryView : View {
+struct FusenWidgetEntryView: View {
     var entry: Provider.Entry
 
     var body: some View {
@@ -49,21 +53,21 @@ struct fusenWidgetEntryView : View {
 }
 
 @main
-struct fusenWidget: Widget {
-    let kind: String = "fusenWidget"
+struct FusenWidget: Widget {
+    private let kind: String = "fusen-widget"
 
     var body: some WidgetConfiguration {
         IntentConfiguration(kind: kind, intent: ConfigurationIntent.self, provider: Provider()) { entry in
-            fusenWidgetEntryView(entry: entry)
+            FusenWidgetEntryView(entry: entry)
         }
         .configurationDisplayName("My Widget")
         .description("This is an example widget.")
     }
 }
 
-struct fusenWidget_Previews: PreviewProvider {
+struct FusenWidget_Previews: PreviewProvider {
     static var previews: some View {
-        fusenWidgetEntryView(entry: SimpleEntry(date: Date(), configuration: ConfigurationIntent()))
+        FusenWidgetEntryView(entry: SimpleEntry(date: Date(), configuration: ConfigurationIntent()))
             .previewContext(WidgetPreviewContext(family: .systemSmall))
     }
 }

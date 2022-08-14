425dac9171e996cfa379a90107dab83c9d8a41f5
gibachan <childhoodend.tk@gmail.com>
2022-08-14 22:05:16 +0900

    Fix action extension to be able to build

--- fusenActionExtension/ActionViewController.swift
+++ fusenActionExtension/ActionViewController.swift
@@ -5,53 +5,39 @@
 //  Created by Tatsuyuki Kobayashi on 2022/08/14.
 //
 
-import UIKit
 import MobileCoreServices
+import UIKit
 import UniformTypeIdentifiers
 
 class ActionViewController: UIViewController {
-
-    @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet private var imageView: UIImageView!
 
     override func viewDidLoad() {
         super.viewDidLoad()
-    
-        // Get the item[s] we're handling from the extension context.
         
-        // For example, look for an image and place it into an image view.
-        // Replace this with something appropriate for the type[s] your extension supports.
-        var imageFound = false
-        for item in self.extensionContext!.inputItems as! [NSExtensionItem] {
-            for provider in item.attachments! {
-                if provider.hasItemConformingToTypeIdentifier(UTType.image.identifier) {
-                    // This is an image. We'll load it, then place it in our image view.
-                    weak var weakImageView = self.imageView
-                    provider.loadItem(forTypeIdentifier: UTType.image.identifier, options: nil, completionHandler: { (imageURL, error) in
-                        OperationQueue.main.addOperation {
-                            if let strongImageView = weakImageView {
-                                if let imageURL = imageURL as? URL {
-                                    strongImageView.image = UIImage(data: try! Data(contentsOf: imageURL))
-                                }
-                            }
-                        }
-                    })
-                    
-                    imageFound = true
-                    break
+        guard let items = self.extensionContext?.inputItems as? [NSExtensionItem] else { return }
+        for item in items {
+            guard let attachments = item.attachments else { continue }
+            for provider in attachments {
+                guard provider.hasItemConformingToTypeIdentifier(UTType.text.identifier) else { continue }
+                provider.loadItem(forTypeIdentifier: UTType.text.identifier, options: nil) { data, error in
+                    print("attributedTitle=\(item.attributedTitle)") // null
+                    print("attributedContentText=\(item.attributedContentText)") // null
+                    print("data=\(data)") // コピーしたテキスト
                 }
             }
             
-            if (imageFound) {
-                // We only handle one image, so stop looking for more.
-                break
-            }
+//            if imageFound {
+//                // We only handle one image, so stop looking for more.
+//                break
+//            }
         }
     }
 
-    @IBAction func done() {
+    @IBAction private func done() {
+        print("done")
         // Return any edited content to the host app.
         // This template doesn't do anything, so we just echo the passed in items.
         self.extensionContext!.completeRequest(returningItems: self.extensionContext!.inputItems, completionHandler: nil)
     }
-
 }

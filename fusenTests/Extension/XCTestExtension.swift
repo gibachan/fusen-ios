8c25060c7742b9924b555cfa16f78f64bb100be8
gibachan <childhoodend.tk@gmail.com>
2023-03-24 11:56:22 +0900

    Add books count on book tab

---
+++ fusenTests/Extension/XCTestExtension.swift
@@ -0,0 +1,26 @@
+//
+//  XCTestExtension.swift
+//  fusenTests
+//
+//  Created by Tatsuyuki Kobayashi on 2023/03/24.
+//
+
+import XCTest
+
+extension XCTest {
+    // ref: Meet async/await in Swift WWDC 2021 session
+    func XCTAssertThrowsError<T: Sendable>(
+        _ expression: @autoclosure () async throws -> T,
+        _ message: @autoclosure () -> String = "",
+        file: StaticString = #filePath,
+        line: UInt = #line,
+        _ errorHandler: (_ error: Error) -> Void = { _ in }
+    ) async {
+        do {
+            _ = try await expression()
+            XCTFail(message(), file: file, line: line)
+        } catch {
+            errorHandler(error)
+        }
+    }
+}
